4aa7494c8b37db8dcaedb9c864015db32c49c446
Sergey Korenyugin <korenb23@mail.ru>
2019-07-31 11:21:50 +0500

    vue create

---
+++ movies-viewer/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MovieList />
+  </div>
+</template>
+
+<script>
+import MovieList from "./components/MovieList.vue";
+
+export default {
+  name: "app",
+  components: {
+    MovieList
+  }
+};
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1050px;
+}
+</style>
---
+++ movies-viewer/src/components/MovieList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Cписок фильмов</h1>
+</template>
+
+<script>
+export default {
+  name: "MovieList",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style>
+</style>
