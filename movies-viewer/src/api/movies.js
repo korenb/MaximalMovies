f58003c565e1cd85c0ea4c69124e154534587d0a
Sergey Korenyugin <korenb23@mail.ru>
2019-07-31 23:12:08 +0500

    dockerized web app

--- movies-viewer/src/api/movies.js
+++ movies-viewer/src/api/movies.js
@@ -1,10 +1,11 @@
 
+const ApiUrl = process.env.VUE_APP_API_MOVIES_URL;
 
 export async function loadMovies(take, skip = null) {
     let query = `?take=${take}`;
     if (skip) query += '&skip=' + skip;
 
-    let response = await fetch('http://localhost:5000/api/movies' + query, {
+    let response = await fetch(ApiUrl + 'api/movies' + query, {
         method: 'GET'
     });
 
@@ -14,7 +15,7 @@ export async function loadMovies(take, skip = null) {
 }
 
 export async function loadMovie(id) {
-    let response = await fetch('http://localhost:5000/api/movies/' + id, {
+    let response = await fetch(ApiUrl + 'api/movies/' + id, {
         method: 'GET'
     });
 
